a06a2ab4f679024f5c0af0cf1001780ce8e06c4b
Ian de Jesus <ian@eskwelabs.com>
2022-01-01 07:38:52 +0900

    feat: add gql-request and bootstrap JIRA: https://insiderinvesting.atlassian.net/browse/INSD-67

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -2,15 +2,33 @@ import React, { useState, useEffect } from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+import { request, gql } from 'graphql-request';
 
 const regex = /collection\/(.*)/;
-
+const GET_POKEMONS = gql`
+  query pokemons($limit: Int, $offset: Int) {
+    pokemons(limit: $limit, offset: $offset) {
+      count
+      next
+      previous
+      status
+      message
+      results {
+        url
+        name
+        image
+      }
+    }
+  }
+`;
 const Popup = () => {
   const [color, setColor] = useState();
   const [URL, setURL] = useState();
   const [collection, setCollection] = useState();
   const [emailAddress, setEmailAddress] = useState();
-
+  // eslint-disable-next-line no-undef
+  // const name = faker.name.findName(); // Caitlyn Kerluke
+  // console.log(name);
   const handleEmailChange = (e) => {
     chrome.storage.sync.set({ emailAddress: e.target.value });
     setEmailAddress(e.target.value);
@@ -51,13 +69,21 @@ const Popup = () => {
     }
   }, [URL]);
 
+  useEffect(() => {
+    request('https://graphql-pokeapi.graphcdn.app/', GET_POKEMONS).then(
+      (data) => console.log(data)
+    );
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <div>URL: {URL}</div>
         <div>Collection: {collection}</div>
         <div>Email: {emailAddress}</div>
-        <button onClick={changeColor}>Change Background Color</button>
+        <button className="btn btn-primary" onClick={changeColor}>
+          Change Background Color
+        </button>
         <input
           type="text"
           value={emailAddress}
