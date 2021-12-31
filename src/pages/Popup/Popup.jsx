2a0b4259f5bb876bdbf3d375a1781ee17e389440
Ian de Jesus <ian@eskwelabs.com>
2021-12-31 13:33:41 +0900

    feat: implement basic chrome extension fn (change background color)

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
-
 const Popup = () => {
+  const [color, setColor] = useState();
+
+  useEffect(() => {
+    chrome.storage.sync.get('color', ({ color }) => {
+      setColor(color);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        <p>{color}</p>
+        <button onClick={changeColor}>Change Background Color</button>
       </header>
     </div>
   );
 };
 
 export default Popup;
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+const changeColor = async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+};
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get('color', ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
